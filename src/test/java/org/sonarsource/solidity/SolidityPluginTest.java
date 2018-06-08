1c6fdf5baab1a1c8c43112869840f3771e953828
stylianos-agapiou-sonarsource <stylianos.agapiou@sonarsource.com>
2018-06-08 16:43:04 +0200

    Add simple test case

--- src/test/java/org/sonarsource/solidity/SolidityPluginTest.java
+++ src/test/java/org/sonarsource/solidity/SolidityPluginTest.java
@@ -31,6 +31,12 @@ public class SolidityPluginTest {
     assertThat(context.getExtensions()).as("Number of extensions for SQ 6.7").hasSize(5);
   }
 
+  @Test
+  public void count_plugin_extensions_sq7_2() {
+    Plugin.Context context = setupContext(SonarRuntimeImpl.forSonarQube(Version.create(7, 2), SonarQubeSide.SERVER));
+    assertThat(context.getExtensions()).as("Number of extensions for SQ 7.2").hasSize(5);
+  }
+
   private Plugin.Context setupContext(SonarRuntime runtime) {
     Plugin.Context context = new Plugin.Context(runtime);
     new SolidityPlugin().define(context);
