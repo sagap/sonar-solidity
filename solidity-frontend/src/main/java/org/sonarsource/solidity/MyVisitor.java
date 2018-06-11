4358db48db8455629d5dc571759bfe5beb7fd5f4
stylianos-agapiou-sonarsource <stylianos.agapiou@sonarsource.com>
2018-06-11 18:02:47 +0200

    Add test for parsing

--- solidity-frontend/src/main/java/org/sonarsource/solidity/MyVisitor.java
+++
@@ -1,7 +0,0 @@
-package org.sonarsource.solidity;
-
-import org.antlr.v4.runtime.Token;
-
-public class MyVisitor extends SolidityBaseVisitor<Token> {
-
-}
--- solidity-frontend/src/test/java/org/sonarsource/solidity/SolidityParserTest.java
+++ solidity-frontend/src/test/java/org/sonarsource/solidity/SolidityParserTest.java
@@ -4,14 +4,22 @@ import java.util.List;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.TokenStream;
 import org.junit.Test;
+import org.sonarsource.solidity.SolidityParser.ContractDefinitionContext;
+import org.sonarsource.solidity.SolidityParser.ContractPartContext;
+import org.sonarsource.solidity.SolidityParser.IdentifierContext;
+import org.sonarsource.solidity.SolidityParser.PragmaDirectiveContext;
+import org.sonarsource.solidity.SolidityParser.SourceUnitContext;
+import org.sonarsource.solidity.SolidityParser.StructDefinitionContext;
+import org.sonarsource.solidity.SolidityParser.VariableDeclarationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolidityParserTest {
 
   @Test
-  public void test() {
+  public void test_parsing() {
     String test = "pragma solidity ^0.4.22;\n" +
       "\n" +
       "/// @title Voting with delegation.\n" +
@@ -26,12 +34,38 @@ public class SolidityParserTest {
       " uint vote; // index of the voted proposal\n" +
       " }\n" +
       "}";
+    //
     CharStream cs = CharStreams.fromString(test);
     SolidityLexer sl = new SolidityLexer(cs);
-    List<Token> ts = (List<Token>) sl.getAllTokens();
     TokenStream tokens = new CommonTokenStream(sl);
     SolidityParser parser = new SolidityParser(tokens);
 
+    // ... source unit context is the root of the parse tree
+    SourceUnitContext suc = parser.sourceUnit();
+
+    PragmaDirectiveContext pdc = suc.pragmaDirective().get(0);
+    assertThat(pdc).isNotNull();
+    ContractDefinitionContext c = suc.contractDefinition().get(0);
+    assertThat(c).isNotNull();
+
+    // in this source unit ctx there is no import directive
+    assertThat(suc.importDirective()).isEmpty();
+
+    // now check that contract definition is well parsed
+    assertThat(c.inheritanceSpecifier()).isEmpty();
+
+    ContractPartContext cp = c.contractPart().get(0);
+    assertThat(cp).isNotNull();
+
+    // now check for contractpart
+    StructDefinitionContext sdc = cp.structDefinition();
+    assertThat(sdc).isNotNull();
+
+    IdentifierContext ic = sdc.identifier();
+    assertThat(ic).isNotNull();
+
+    List<VariableDeclarationContext> vdcList = sdc.variableDeclaration();
+    assertThat(vdcList).hasSize(4);
   }
 
 }
