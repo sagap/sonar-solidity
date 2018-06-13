bc7fad118540f79b13f217c6335707108e210814
stylianos-agapiou-sonarsource <36889190+stylianos-agapiou-sonarsource@users.noreply.github.com>
2018-06-13 10:46:28 +0200

    Fix duplication (#13)

--- solidity-frontend/src/main/java/org/sonarsource/solidity/frontend/SolidityParser.java
+++ solidity-frontend/src/main/java/org/sonarsource/solidity/frontend/SolidityParser.java
@@ -10,8 +10,6 @@ import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.RuntimeMetaData;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.TokenStream;
-import org.antlr.v4.runtime.Vocabulary;
-import org.antlr.v4.runtime.VocabularyImpl;
 // Generated from Solidity.g4 by ANTLR 4.7
 import org.antlr.v4.runtime.atn.ATN;
 import org.antlr.v4.runtime.atn.ATNDeserializer;
@@ -101,89 +99,28 @@ public class SolidityParser extends Parser {
     "elementaryTypeNameExpression", "numberLiteral", "identifier"
   };
 
-  private static final String[] _LITERAL_NAMES = {
-    null, "'pragma'", "';'", "'^'", "'~'", "'>='", "'>'", "'<'", "'<='", "'='",
-    "'as'", "'import'", "'*'", "'from'", "'{'", "','", "'}'", "'contract'",
-    "'interface'", "'library'", "'is'", "'('", "')'", "'using'", "'for'",
-    "'struct'", "'constructor'", "'modifier'", "'function'", "'returns'",
-    "'event'", "'enum'", "'['", "']'", "'.'", "'mapping'", "'=>'", "'memory'",
-    "'storage'", "'if'", "'else'", "'while'", "'assembly'", "'do'", "'return'",
-    "'throw'", "'emit'", "'var'", "'address'", "'bool'", "'string'", "'byte'",
-    "'++'", "'--'", "'new'", "'+'", "'-'", "'after'", "'delete'", "'!'", "'**'",
-    "'/'", "'%'", "'<<'", "'>>'", "'&'", "'|'", "'=='", "'!='", "'&&'", "'||'",
-    "'?'", "':'", "'|='", "'^='", "'&='", "'<<='", "'>>='", "'+='", "'-='",
-    "'*='", "'/='", "'%='", "'let'", "':='", "'=:'", "'switch'", "'case'",
-    "'default'", "'->'", null, null, null, null, null, null, null, null, null,
-    null, null, null, "'anonymous'", "'break'", "'constant'", "'continue'",
-    "'external'", "'indexed'", "'internal'", "'payable'", "'private'", "'public'",
-    "'pure'", "'view'"
-  };
-  private static final String[] _SYMBOLIC_NAMES = {
-    null, null, null, null, null, null, null, null, null, null, null, null,
-    null, null, null, null, null, null, null, null, null, null, null, null,
-    null, null, null, null, null, null, null, null, null, null, null, null,
-    null, null, null, null, null, null, null, null, null, null, null, null,
-    null, null, null, null, null, null, null, null, null, null, null, null,
-    null, null, null, null, null, null, null, null, null, null, null, null,
-    null, null, null, null, null, null, null, null, null, null, null, null,
-    null, null, null, null, null, null, "Int", "Uint", "Byte", "Fixed", "Ufixed",
-    "VersionLiteral", "BooleanLiteral", "DecimalNumber", "HexNumber", "NumberUnit",
-    "HexLiteral", "ReservedKeyword", "AnonymousKeyword", "BreakKeyword", "ConstantKeyword",
-    "ContinueKeyword", "ExternalKeyword", "IndexedKeyword", "InternalKeyword",
-    "PayableKeyword", "PrivateKeyword", "PublicKeyword", "PureKeyword", "ViewKeyword",
-    "Identifier", "StringLiteral", "WS", "COMMENT", "LINE_COMMENT"
-  };
-  public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
-
-  /**
-   * @deprecated Use {@link #VOCABULARY} instead.
-   */
-  @Deprecated
-  public static final String[] tokenNames;
-  static {
-    tokenNames = new String[_SYMBOLIC_NAMES.length];
-    for (int i = 0; i < tokenNames.length; i++) {
-      tokenNames[i] = VOCABULARY.getLiteralName(i);
-      if (tokenNames[i] == null) {
-        tokenNames[i] = VOCABULARY.getSymbolicName(i);
-      }
-
-      if (tokenNames[i] == null) {
-        tokenNames[i] = "<INVALID>";
-      }
-    }
-  }
-
-  @Override
-  @Deprecated
-  public String[] getTokenNames() {
-    return tokenNames;
-  }
-
-  @Override
-
-  public Vocabulary getVocabulary() {
-    return VOCABULARY;
-  }
-
   @Override
   public String getGrammarFileName() {
-    return "Solidity.g4";
+    return new SolidityTokensInfo().getGrammarFileName();
   }
 
   @Override
   public String[] getRuleNames() {
-    return ruleNames;
+    return new SolidityTokensInfo().getRuleNames();
   }
 
   @Override
-  public String getSerializedATN() {
-    return _serializedATN;
+  public String[] getTokenNames() {
+    return new SolidityTokensInfo().getTokenNames();
   }
 
   @Override
   public ATN getATN() {
     return _ATN;
+  }
+
+  public String getSerializedATN() {
+    return _serializedATN;
   }
 
   public SolidityParser(TokenStream input) {
---
+++ solidity-frontend/src/main/java/org/sonarsource/solidity/frontend/SolidityTokensInfo.java
@@ -0,0 +1,98 @@
+package org.sonarsource.solidity.frontend;
+
+import org.antlr.v4.runtime.Vocabulary;
+import org.antlr.v4.runtime.VocabularyImpl;
+
+public class SolidityTokensInfo {
+
+  protected static final String GRAMMAR_FILENAME = "Solidity.g4";
+
+  protected static final String[] ruleNames = {
+    "sourceUnit", "pragmaDirective", "pragmaName", "pragmaValue", "version",
+    "versionOperator", "versionConstraint", "importDeclaration", "importDirective",
+    "contractDefinition", "inheritanceSpecifier", "contractPart", "stateVariableDeclaration",
+    "usingForDeclaration", "structDefinition", "constructorDefinition", "modifierDefinition",
+    "modifierInvocation", "functionDefinition", "returnParameters", "modifierList",
+    "eventDefinition", "enumValue", "enumDefinition", "parameterList", "parameter",
+    "eventParameterList", "eventParameter", "functionTypeParameterList", "functionTypeParameter",
+    "variableDeclaration", "typeName", "userDefinedTypeName", "mapping", "functionTypeName",
+    "storageLocation", "stateMutability", "block", "statement", "expressionStatement",
+    "ifStatement", "whileStatement", "simpleStatement", "forStatement", "inlineAssemblyStatement",
+    "doWhileStatement", "continueStatement", "breakStatement", "returnStatement",
+    "throwStatement", "emitStatement", "variableDeclarationStatement", "identifierList",
+    "elementaryTypeName", "expression", "primaryExpression", "expressionList",
+    "nameValueList", "nameValue", "functionCallArguments", "functionCall",
+    "assemblyBlock", "assemblyItem", "assemblyExpression", "assemblyCall",
+    "assemblyLocalDefinition", "assemblyAssignment", "assemblyIdentifierOrList",
+    "assemblyIdentifierList", "assemblyStackAssignment", "labelDefinition",
+    "assemblySwitch", "assemblyCase", "assemblyFunctionDefinition", "assemblyFunctionReturns",
+    "assemblyFor", "assemblyIf", "assemblyLiteral", "subAssembly", "tupleExpression",
+    "elementaryTypeNameExpression", "numberLiteral", "identifier"
+  };
+
+  private static final String[] _LITERAL_NAMES = {
+    null, "'pragma'", "';'", "'^'", "'~'", "'>='", "'>'", "'<'", "'<='", "'='",
+    "'as'", "'import'", "'*'", "'from'", "'{'", "','", "'}'", "'contract'",
+    "'interface'", "'library'", "'is'", "'('", "')'", "'using'", "'for'",
+    "'struct'", "'constructor'", "'modifier'", "'function'", "'returns'",
+    "'event'", "'enum'", "'['", "']'", "'.'", "'mapping'", "'=>'", "'memory'",
+    "'storage'", "'if'", "'else'", "'while'", "'assembly'", "'do'", "'return'",
+    "'throw'", "'emit'", "'var'", "'address'", "'bool'", "'string'", "'byte'",
+    "'++'", "'--'", "'new'", "'+'", "'-'", "'after'", "'delete'", "'!'", "'**'",
+    "'/'", "'%'", "'<<'", "'>>'", "'&'", "'|'", "'=='", "'!='", "'&&'", "'||'",
+    "'?'", "':'", "'|='", "'^='", "'&='", "'<<='", "'>>='", "'+='", "'-='",
+    "'*='", "'/='", "'%='", "'let'", "':='", "'=:'", "'switch'", "'case'",
+    "'default'", "'->'", null, null, null, null, null, null, null, null, null,
+    null, null, null, "'anonymous'", "'break'", "'constant'", "'continue'",
+    "'external'", "'indexed'", "'internal'", "'payable'", "'private'", "'public'",
+    "'pure'", "'view'"
+  };
+  private static final String[] _SYMBOLIC_NAMES = {
+    null, null, null, null, null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, "Int", "Uint", "Byte", "Fixed", "Ufixed",
+    "VersionLiteral", "BooleanLiteral", "DecimalNumber", "HexNumber", "NumberUnit",
+    "HexLiteral", "ReservedKeyword", "AnonymousKeyword", "BreakKeyword", "ConstantKeyword",
+    "ContinueKeyword", "ExternalKeyword", "IndexedKeyword", "InternalKeyword",
+    "PayableKeyword", "PrivateKeyword", "PublicKeyword", "PureKeyword", "ViewKeyword",
+    "Identifier", "StringLiteral", "WS", "COMMENT", "LINE_COMMENT"
+  };
+  public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+  protected static final String[] tokenNames;
+
+  static {
+    tokenNames = new String[_SYMBOLIC_NAMES.length];
+    for (int i = 0; i < tokenNames.length; i++) {
+      tokenNames[i] = VOCABULARY.getLiteralName(i);
+      if (tokenNames[i] == null) {
+        tokenNames[i] = VOCABULARY.getSymbolicName(i);
+      }
+      if (tokenNames[i] == null) {
+        tokenNames[i] = "<INVALID>";
+      }
+    }
+  }
+
+  public String[] getTokenNames() {
+    return tokenNames;
+  }
+
+  public Vocabulary getVocabulary() {
+    return VOCABULARY;
+  }
+
+  public String getGrammarFileName() {
+    return GRAMMAR_FILENAME;
+  }
+
+  public String[] getRuleNames() {
+    return ruleNames;
+  }
+
+}
