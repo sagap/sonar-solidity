36c9a787fc6bb4122bb4ca57fd06e2af0aabf674
stylianos-agapiou-sonarsource <stylianos.agapiou@sonarsource.com>
2018-07-05 10:47:47 +0200

    Improve coverage for latest version rule

--- solidity-checks/src/test/resources/LatestVersionCheck/test.sol
+++ solidity-checks/src/test/resources/LatestVersionCheck/test.sol
@@ -1,2 +1,5 @@
    pragma solidity ^0.4.16;  // Noncompliant {{Not the latest version of solidity}}
 // ^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+      pragma solidity ^0.4.19 <=0.4.22;  // Noncompliant {{Not the latest version of solidity}}
--- solidity-checks/src/test/resources/LatestVersionCheck/test1.sol
+++ solidity-checks/src/test/resources/LatestVersionCheck/test1.sol
@@ -1,1 +1,8 @@
    pragma solidity ^0.4.24;  // Compliant
+   
+   
+   pragma solidity ^0.4.19 <=0.4.24;  // Compliant
+   
+   pragma solidity >=0.4.18;  // Compliant
+   
+   pragma solidity 0.4.25; // Compliant
