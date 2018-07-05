4e8a08398b3a3769c1a903ddcfafe6865ba59f6d
stylianos-agapiou-sonarsource <36889190+stylianos-agapiou-sonarsource@users.noreply.github.com>
2018-07-05 15:44:12 +0200

    Find differences after analysis (#31)

--- solidity-its/src/main/java/org/sonarsource/solidity/its/SolidityRuling.java
+++ solidity-its/src/main/java/org/sonarsource/solidity/its/SolidityRuling.java
@@ -3,10 +3,18 @@ package org.sonarsource.solidity.its;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.sonar.api.batch.rule.ActiveRules;
 import org.sonar.api.batch.rule.CheckFactory;
@@ -25,11 +33,16 @@ public class SolidityRuling {
 
   private static final Logger LOG = Loggers.get(SolidityRuling.class);
 
+  private static final String DIR = "solidity-test-sources/src/";
+  protected static final String DIFFERENCES = "src/test/resources/differences";
+
+  protected static Map<String, List<File>> filesToAnalyze = new LinkedHashMap<>();
+  protected static Map<String, List<File>> filesToCompare = new LinkedHashMap<>();
+
   private SolidityRuling() {
     // ... no reason for a public constructor
   }
 
-  private static final String DIR = "solidity-test-sources/src/";
   private static final String[] PROJECTS_TO_ANALYZE = {
     "ethereum-api",
     "Random-Files"
@@ -43,17 +56,63 @@ public class SolidityRuling {
     Arrays.asList(getProjects())
       .stream()
       .forEach(projectName -> {
+        List<File> listFiles = new ArrayList<>();
         try {
           String path = String.format("%s%s", DIR, projectName);
           Files.find(Paths.get(path),
             Integer.MAX_VALUE,
             (filePath, fileAttr) -> fileAttr.isRegularFile())
             .filter(file -> file.getFileName().toString().endsWith(".sol"))
-            .forEach(file -> analyzeFile(file.toFile(), projectName));
+            .forEach(file -> listFiles.add(file.toFile()));
+          sortList(listFiles);
+          filesToAnalyze.put(projectName, listFiles);
         } catch (IOException e) {
           LOG.debug(e.getMessage(), e);
         }
       });
+  }
+
+  public static void findDifferences() {
+    filesToCompare.forEach((projectName, recordedIssues) -> {
+      String actualIssuePath = String.format("%s%s", SolidityRulingIts.ACTUAL_ISSUES,
+        recordedIssues.toString().replaceAll(SolidityRulingIts.RECORD_ISSUES, ""));
+      try {
+        if (!FileUtils.contentEquals(new File(recordedIssues.toString()), new File(actualIssuePath))) {
+          List<String> lines = Arrays.asList("Differences: " + recordedIssues.toString() + " - " + actualIssuePath);
+          Files.write(Paths.get(String.format("%s%s", SolidityRulingIts.RECORD_ISSUES, "differences")), lines, StandardCharsets.UTF_8,
+            StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        }
+      } catch (IOException e) {
+        LOG.debug(e.getMessage(), e);
+      }
+    });
+  }
+
+  public static void analyzeFiles() {
+    filesToAnalyze.forEach((projectName, fileList) -> {
+      fileList.stream().forEach(file -> {
+        analyzeFile(file, projectName);
+      });
+    });
+  }
+
+  public static void collectFilesForIssues() {
+    Arrays.asList(getProjects())
+      .stream()
+      .forEach(projectName -> {
+        String path = String.format("%s%s", SolidityRulingIts.RECORD_ISSUES, projectName);
+        List<File> listFiles = new ArrayList<>();
+        try {
+          Files.find(Paths.get(path),
+            Integer.MAX_VALUE,
+            (filePath, fileAttr) -> fileAttr.isRegularFile())
+            .forEach(recordedIssues -> listFiles.add(recordedIssues.toFile()));
+          filesToCompare.put(projectName, listFiles);
+        } catch (IOException e) {
+          LOG.debug(e.getMessage(), e);
+        }
+      });
+
   }
 
   private static void analyzeFile(File file, String projectName) {
@@ -71,26 +130,17 @@ public class SolidityRuling {
     }
   }
 
-  public static void deletePreviouslyAnalyzedFiles() {
-    Arrays.asList(getProjects())
-      .stream()
-      .forEach(projectName -> {
-        String path = String.format("%s%s", SolidityRulingIts.RECORD_ISSUES, projectName);
+  public static void deletePreviouslyAnalyzedFiles() throws IOException {
+    filesToCompare.forEach((projectName, fileList) -> {
+      fileList.stream().forEach(file -> {
         try {
-          Files.find(Paths.get(path),
-            Integer.MAX_VALUE,
-            (filePath, fileAttr) -> fileAttr.isRegularFile())
-            .forEach(t -> {
-              try {
-                Files.deleteIfExists(t);
-              } catch (IOException e) {
-                LOG.debug(e.getMessage(), e);
-              }
-            });
+          Files.deleteIfExists(file.toPath());
         } catch (IOException e) {
           LOG.debug(e.getMessage(), e);
         }
       });
+    });
+    Files.deleteIfExists(new File(DIFFERENCES).toPath());
   }
 
   private static Collection<IssuableVisitor> collectVisitors() {
@@ -99,6 +149,10 @@ public class SolidityRuling {
     return checkFactory.<IssuableVisitor>create(SolidityRulesDefinition.REPO_KEY)
       .addAnnotatedChecks((Iterable) CheckList.returnChecks())
       .all();
+  }
+
+  private static void sortList(List<File> listFiles) {
+    Collections.sort(listFiles, (File f1, File f2) -> f1.getName().compareTo(f2.getName()));
   }
 
   private static ActiveRules activeRules() {
--- solidity-its/src/test/java/org/sonarsource/solidity/its/SolidityRulingTest.java
+++ solidity-its/src/test/java/org/sonarsource/solidity/its/SolidityRulingTest.java
@@ -1,15 +1,36 @@
 package org.sonarsource.solidity.its;
 
+import java.io.File;
+import java.io.IOException;
 import org.junit.Test;
+import org.sonar.api.utils.log.Logger;
+import org.sonar.api.utils.log.Loggers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolidityRulingTest {
 
+  private static final Logger LOG = Loggers.get(SolidityRulingTest.class);
+
   @Test
   public void test() {
     assertThat(SolidityRuling.getProjects()).hasSize(2);
-    SolidityRuling.deletePreviouslyAnalyzedFiles();
-    SolidityRuling.collectSolidityFiles();
+    try {
+      SolidityRuling.collectFilesForIssues();
+
+      SolidityRuling.deletePreviouslyAnalyzedFiles();
+
+      File file = new File(SolidityRuling.DIFFERENCES);
+      assertThat(file).doesNotExist();
+
+      SolidityRuling.collectSolidityFiles();
+      SolidityRuling.analyzeFiles();
+
+      SolidityRuling.findDifferences();
+
+      assertThat(file).doesNotExist();
+    } catch (IOException e) {
+      LOG.debug(e.getMessage());
+    }
   }
 }
