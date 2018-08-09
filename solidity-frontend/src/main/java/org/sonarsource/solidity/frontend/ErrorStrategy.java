0adb9abcf3b2d65b5e0e323fc0dfe07480f3c019
stylianos-agapiou-sonarsource <36889190+stylianos-agapiou-sonarsource@users.noreply.github.com>
2018-08-09 10:49:19 +0200

    improve coverage, small fixes (#88)

--- solidity-frontend/src/main/java/org/sonarsource/solidity/frontend/ErrorStrategy.java
+++ solidity-frontend/src/main/java/org/sonarsource/solidity/frontend/ErrorStrategy.java
@@ -4,7 +4,6 @@ import org.antlr.v4.runtime.DefaultErrorStrategy;
 import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.misc.IntervalSet;
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 
@@ -17,19 +16,6 @@ public class ErrorStrategy extends DefaultErrorStrategy {
     Token t = recognizer.getCurrentToken();
     String errorMessage = String.format(
       "Unexpected parsing error occurred. Last found valid token: %s at position %s:%s",
-      getTokenErrorDisplay(t),
-      t.getLine(),
-      t.getCharPositionInLine());
-    LOG.debug(errorMessage);
-  }
-
-  @Override
-  protected void reportMissingToken(Parser recognizer) {
-    Token t = recognizer.getCurrentToken();
-    IntervalSet expecting = getExpectedTokens(recognizer);
-    String errorMessage = String.format(
-      "missing %s before %s at position %s:%s",
-      expecting.toString(recognizer.getVocabulary()),
       getTokenErrorDisplay(t),
       t.getLine(),
       t.getCharPositionInLine());
